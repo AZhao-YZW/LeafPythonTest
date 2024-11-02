8b95e29d5030cdcb61124857485013edb937008b
AZhao <yuzhaowei2002@outlook.com>
2024-11-02 20:57:41 +0800

    add test_core option NEW, DEL llt

--- llt/core_test/testcore_test/src/test_test_core.cc
+++ llt/core_test/testcore_test/src/test_test_core.cc
@@ -6,13 +6,19 @@ extern "C" {
 #include "test_data.h"
 #include "error_code.h"
 #include "leafpy_cfg.h"
-int test_core_run_op_proc(test_core_op_info_s *op_info, test_core_s *core);
 }
 
 class TestTestCore : public testing::Test {
 protected:
+    test_core_op_info_s op_info = {0};
+
     virtual void SetUp()
     {
+        test_core_op_info_s tmp_op_info = {0};
+        int ret;
+        ret = test_core_init(LEAFPY_DEFAULT_CORE_ID);
+        ASSERT_EQ(ret, EC_OK);
+        op_info = tmp_op_info;
     }
 
     virtual void TearDown()
@@ -26,9 +32,6 @@ TEST_F(TestTestCore, core_init)
     test_core_s *core = NULL;
     global_obj_s *global_obj = NULL;
     int ret;
-
-    ret = test_core_init(LEAFPY_DEFAULT_CORE_ID);
-    ASSERT_EQ(ret, EC_OK);
 
     core = test_core_get_core(LEAFPY_DEFAULT_CORE_ID);
     ASSERT_NE(core, (void *)NULL);
@@ -46,20 +49,83 @@ TEST_F(TestTestCore, core_init)
     EXPECT_STREQ(global_obj->obj_attr.obj_name, "global_obj");
 }
 
-TEST_F(TestTestCore, op_test_NEW)
+TEST_F(TestTestCore, core_run_core_id_unexist)
 {
-    test_core_op_info_s op_info = {0};
-    test_core_s *core = NULL;
+    int ret = test_core_run(LEAFPY_DEFAULT_CORE_ID + 1, &op_info);
+    EXPECT_EQ(ret, EC_CORE_ID_INVALID);
+}
+
+TEST_F(TestTestCore, core_run_unsupport_op)
+{
+    op_info.op = TEST_CORE_OP_MAX;
+    int ret = test_core_run(LEAFPY_DEFAULT_CORE_ID, &op_info);
+    EXPECT_EQ(ret, EC_UNSUPPORT_OP);
+}
+
+static void test_core_run_op_NEW_success(test_core_op_info_s *op_info)
+{
+    char test_obj_name[] = "test_obj";
     int ret;
 
-    ret = test_core_init(LEAFPY_DEFAULT_CORE_ID);
-    ASSERT_EQ(ret, EC_OK);
+    op_info->op = TEST_CORE_OP_NEW;
+    op_info->info.op_new.obj_type = OBJ_TYPE_OBJECT;
+    op_info->info.op_new.obj_name = test_obj_name;
+    op_info->info.op_new.obj_name_len = sizeof(test_obj_name);
+    op_info->info.op_new.parent_id = ROOT_OBJ_ID;
+    ret = test_core_run(LEAFPY_DEFAULT_CORE_ID, op_info);
+    EXPECT_EQ(ret, EC_OK);
+}
 
-    core = test_core_get_core(LEAFPY_DEFAULT_CORE_ID);
-    ASSERT_NE(core, (void *)NULL);
+TEST_F(TestTestCore, core_run_op_NEW_success)
+{
+    test_core_run_op_NEW_success(&op_info);
+}
+
+TEST_F(TestTestCore, core_run_op_NEW_obj_name_len_exceed)
+{
+    char test_obj_name[] = "test_obj";
+    int ret;
 
     op_info.op = TEST_CORE_OP_NEW;
     op_info.info.op_new.obj_type = OBJ_TYPE_OBJECT;
-    ret = test_core_run_op_proc(&op_info, core);
+    op_info.info.op_new.obj_name = test_obj_name;
+    op_info.info.op_new.obj_name_len = LEAFPY_MAX_OBJ_NAME_LEN + 1;
+    op_info.info.op_new.parent_id = ROOT_OBJ_ID;
+    ret = test_core_run(LEAFPY_DEFAULT_CORE_ID, &op_info);
+    EXPECT_EQ(ret, EC_OBJ_NAME_LEN_EXCEED);
+}
+
+TEST_F(TestTestCore, op_test_DEL_success)
+{
+    int ret;
+
+    test_core_run_op_NEW_success(&op_info);
+    op_info.op = TEST_CORE_OP_DEL;
+    op_info.info.op_del.obj_id = 2;
+    ret = test_core_run(LEAFPY_DEFAULT_CORE_ID, &op_info);
     EXPECT_EQ(ret, EC_OK);
+}
+
+TEST_F(TestTestCore, op_test_DEL_obj_id_unexist)
+{
+    int ret;
+
+    op_info.op = TEST_CORE_OP_DEL;
+    op_info.info.op_del.obj_id = 2;
+    ret = test_core_run(LEAFPY_DEFAULT_CORE_ID, &op_info);
+    EXPECT_EQ(ret, EC_OBJ_ID_INVALID);
+}
+
+TEST_F(TestTestCore, op_test_DEL_obj_id_not_deletable)
+{
+    int ret;
+
+    op_info.op = TEST_CORE_OP_DEL;
+    op_info.info.op_del.obj_id = 0;
+    ret = test_core_run(LEAFPY_DEFAULT_CORE_ID, &op_info);
+    EXPECT_EQ(ret, EC_OBJ_NOT_DELETABLE);
+
+    op_info.info.op_del.obj_id = 1;
+    ret = test_core_run(LEAFPY_DEFAULT_CORE_ID, &op_info);
+    EXPECT_EQ(ret, EC_OBJ_NOT_DELETABLE);
 }
