f9c5786e689f15c9ea70031d9d2c49c25df9d2e6
AZhao <yuzhaowei2002@outlook.com>
2024-11-13 02:28:24 +0800

    add parser llt

---
+++ llt/core_test/testcore_test/src/test_test_parser.cc
@@ -0,0 +1,46 @@
+#include "gtest/gtest.h"
+#include "mockcpp/mokc.h"
+#include <string.h>
+
+extern "C" {
+#include "error_code.h"
+#include "leafpy_cfg.h"
+#include "test_parser.h"
+}
+
+class TestTestParser : public testing::Test {
+protected:
+    test_frame_callback_s frame_cb;
+
+    static int run_frame_stub(u8 core_id, test_frame_s *frame)
+    {
+        return EC_OK;
+    }
+
+    virtual void SetUp()
+    {
+        int ret;
+        frame_cb.run_frame = run_frame_stub;
+        ret = test_frame_register(TEST_FRAME_QUEUE_0, LEAFPY_DEFAULT_CORE_ID, &frame_cb);
+        ASSERT_EQ(ret, EC_OK);
+    }
+
+    virtual void TearDown()
+    {
+        GlobalMockObject::verify();
+    }
+};
+
+TEST_F(TestTestParser, parser_parse_code_success)
+{
+    char code[] = "a = 1";
+    int ret = test_parser_parse_code(code, strlen(code));
+    EXPECT_EQ(ret, EC_OK);
+}
+
+TEST_F(TestTestParser, parser_parse_code_null)
+{
+    char code[] = "a = 1";
+    int ret = test_parser_parse_code(NULL, strlen(code));
+    EXPECT_EQ(ret, EC_PARAM_INVALID);
+}
